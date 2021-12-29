e60d94924c5e5357e7d0874fdb593f2f7e1461fa
hemagazzar <94261285+hemagazzar@users.noreply.github.com>
2021-12-29 23:53:37 +0200

    Update code.asm

--- code.asm
+++ code.asm
@@ -1,1 +1,27 @@
 include 'emu8086.inc' ;come functions library
+
+
+
+
+;-------------- Starting the program --------------
+        
+; Displays welcome message
+LEA DX, welcome_msg
+MOV AH, 9
+INT 21h
+    
+
+LEA DX, welcome_msg2
+MOV AH, 9
+INT 21h
+
+
+
+
+
+welcome_msg db "Welcome to Encryption and Decryption Platform $"
+welcome_msg2 db 0Dh,0Ah, "====================================================== $"
+
+
+
+end
