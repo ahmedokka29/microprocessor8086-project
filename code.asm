0be528ee589bf5fbef846aecd648a63d476d5d33
hemagazzar <94261285+hemagazzar@users.noreply.github.com>
2022-01-05 23:39:36 +0200

    Update code.asm

--- code.asm
+++ code.asm
@@ -1,27 +1,75 @@
 .MODEL SMALL
 .STACK
 .DATA
-ARR DB 011H,012H,013H,014H,015H
+ARR DB 7 DUP(?);00H,01H,02H,03H,04H,05H,06H,07H,08H,09H,010H,012H,012H,012H,012H,012H,012H,013H,014H,015H
 number DB 0
+SOLVE DB 0
 numberplace db 10
-KEY EQU 012H
-MSG1 DB "KEY IS FOUND AT POSITION "
-RES DB " ","$"
-MSG2 DB 'KEY NOT FOUND!!!!!!!!!!!!!. $'
+KEY EQU 0DH
+MSG1 DB "KEY IS FOUND AT POSITION $"
+MSG2 DB "KEY NOT FOUND!!!!!!!!!!!!!. $"
+MSG3 DB "ENTER SIZE: $"   
+MSG4 DB "ENTER ELEMENT NUMBER  $"
+MSG5 DB "ENTER ELEMENTS SEPARATED BY SPACE $"
+SIZE DB 0
 messageinvalidcharacter db 0ah,0dh,"Invalid Character$"
 messageinputnumber db 0ah,0dh,"Please Input a number $"
 
 
 .CODE
-.STARTUP
+.STARTUP    
 MOV AX,@DATA
 MOV DS,AX
-MOV number,0 
- 
-CALL ENTER ;calling function to take an input
+MOV number,0
+LEA DX,MSG3
+MOV AH,09H
+INT 21H
 
-;start of binary search
-MOV BL,AL    
+CALL ENTER
+MOV SIZE,AL
+
+
+MOV CL,0 
+
+
+
+;array elements begin
+ARR_LOOP: 
+
+CMP CL,SIZE
+JE ARR_END 
+
+
+
+ 
+
+
+LEA DX,MSG4
+MOV AH,09H
+INT 21H
+
+MOV AL,CL
+INC AL
+MOV AH,0
+Call print
+MOV AH,4CH
+
+
+CALL ENTER
+
+MOV CH,0
+MOV SI,CX
+MOV ARR[SI],AL
+INC CL
+JMP ARR_LOOP
+ARR_END:
+;array elements end
+
+                   
+                   
+
+MOV BL,SIZE
+DEC BL    
 
 MOV BH,00
 MOV CL,KEY
@@ -46,27 +94,27 @@ INC AL
 MOV BH,AL
 JMP AGAIN
     
-SUCCESS:ADD AL,01
-ADD AL,'0'
-MOV RES,AL
+SUCCESS:
+MOV SOLVE,AL
 LEA DX,MSG1
-JMP DISP
+MOV AH,09H
+INT 21H
+MOV AL,SOLVE 
+INC AL
+MOV AH,0 
+CALL PRINT
+
+JMP P_END
 
 FAIL:LEA DX,MSG2
 
-DISP:MOV AH,09H
-INT 21H
-MOV AH,4CH  
+  
 JMP P_END
-;end of binary search
-
-;function for taking input
     ENTER PROC NEAR
-    
+        PUSH AX
+        PUSH BX
+        PUSH DX
         loop_number_main:
-        mov ah,09h  
-        mov dx,offset messageinputnumber    
-        int 21h     
         
         mov number,0    
     loop_read_number:
@@ -84,19 +132,23 @@ JMP P_END
      
         cmp al,39h  
         jg invalidcharacter 
-              
+        
+        
         sub al,30h  
                                 
         mov bl,al   
         
         mov al,number   
         mul numberplace  
-                               
+                        
+        
         add al,bl       
         mov number,al
-             
+        
+       
         jmp loop_read_number
-      
+    
+    
 invalidcharacter:
     mov ah,09h  
     mov dx,offset messageinvalidcharacter   
@@ -105,13 +157,79 @@ invalidcharacter:
     jmp loop_number_main           
         
 numbercomplete:
-    
+ 
+    POP DX
+    POP BX
+    POP AX
     mov al,number 
-
-
+    
     RET
-ENTER ENDP
-
+ENTER ENDP  
+    
+    
+    
+    
+PRINT PROC          
+     
+    ;initialize count
+    PUSH AX
+    PUSH BX
+    PUSH CX
+    PUSH DX
+    mov cx,0
+    mov dx,0
+    label1:
+        ; if ax is zero
+        cmp ax,0
+        je print1     
+         
+        ;initialize bx to 10
+        mov bx,10       
+         
+        ; extract the last digit
+        div bx                 
+         
+        ;push it in the stack
+        push dx             
+         
+        ;increment the count
+        inc cx             
+         
+        ;set dx to 0
+        xor dx,dx
+        jmp label1
+    print1:
+        ;check if count
+        ;is greater than zero
+        cmp cx,0
+        je exit
+         
+        ;pop the top of stack
+        pop dx
+         
+        ;add 48 so that it
+        ;represents the ASCII
+        ;value of digits
+        add dx,48
+         
+        ;interrupt to print a
+        ;character
+        mov ah,02h
+        int 21h
+         
+        ;decrease the count
+        dec cx
+        jmp print1
+exit:
+    POP DX
+    POP CX
+    POP BX
+    POP AX
+ret
+PRINT ENDP
+    
+    
+    
 P_END:
 .EXIT
 END
